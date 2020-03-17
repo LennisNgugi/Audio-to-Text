0831e7810e7cd6f48f4ae75b7352fbccd40c9bd7
734415 <42470457+LennisNgugi@users.noreply.github.com>
2020-03-17 21:42:43 +0000

    Delete Auto-to-Text.py

--- Auto-to-Text.py
+++
@@ -1,9 +0,0 @@
-import os 
-import speech_recognition as sr
-from tqdm import tqdm
-
-with open("APT-KEY") as f:
-    GOOGLE_CLOUD_SPEED_CREDENTIALS = f.read()
-
-r = sr.Recognizer()
-files = sorted(os.listdir('parts/'))
